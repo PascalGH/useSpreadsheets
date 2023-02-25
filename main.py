054fb74ace2a52cb0ceeb003b2a3448773251ede
Pascal <pascaldutrieux>
2023-02-25 21:48:39 +0100

    First commit or usage of openpyxl.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Installation command line: python3.11 -m pip install openpyxl
+from openpyxl import load_workbook
 
 
 def print_hi(name):
@@ -11,6 +13,18 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Pascal2')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    wb = load_workbook('Test.xlsx')
+    sheet = wb.active
+    print(sheet["A1"].value)
+    print(sheet["B1"].value)
+    print(sheet["C1"].value)
+    print(sheet["D1"].value)
+    print(sheet.cell(row=1, column=1).value)
+    print(sheet.cell(row=1, column=2).value)
+    print(sheet.cell(row=1, column=3).value)
+    print(sheet.cell(row=1, column=4).value)
+    print(sheet["A1:D2"])
+    for row in sheet["A1:D3"]:
+        print([x.value for x in row])
